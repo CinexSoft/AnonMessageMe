e9699d6d68d9d674420f87a00ed10004cf9841c9
OogleGlu <avirukbasak@gmail.com>
2021-12-12 22:45:40 +0530

    refavtored code: init.js

--- development/public/common/scripts/init.js
+++ development/public/common/scripts/init.js
@@ -1,8 +1,5 @@
-import { Auth, Database, } from '/common/scripts/fbinit.js';
+import { Auth, Database, FirebaseAuth, FirebaseDB, } from '/common/scripts/fbinit.js';
 import { setVariable, USER_ROOT } from '/common/scripts/variables.js';
-
-import * as FirebaseAuth from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js';
-import * as FirebaseDB from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js';
 
 const init = function() {
     
